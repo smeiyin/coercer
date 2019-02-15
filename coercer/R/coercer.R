e431e71a919a8838f04c5414e7177bc87d41a151
Sarah Makonnen <makonnen.sarah@gmail.com>
2019-02-15 19:41:00 +0100

    help page

---
+++ coercer/R/coercer.R
@@ -0,0 +1,11 @@
+#' coercer: A package for coercing multiple variables at once.
+#'
+#' The coercer package provides three categories of important functions:
+#' intonum, intofact and intochar.
+#'
+#' @section Coercer functions:
+#' The coercer functions ...
+#'
+#' @docType package
+#' @name coercer
+NULL
