970bced3126092775d08ac88f2c7c38c419c6c9e
Sarah Makonnen <makonnen.sarah@gmail.com>
2019-02-20 10:20:22 +0100

    Update help page

--- coercer/R/coercer.R
+++ coercer/R/coercer.R
@@ -5,8 +5,11 @@
 #' @section Details:
 #' This package contains several functions to convert multiple variables to
 #' either numeric, factors or characters at once: see \code{\link{intonum}},
-#' \code{\link{intofact}}, and \code{\link{intochar}}
+#' \code{\link{intofact}}, and \code{\link{intochar}}.
 #'
 #' @docType package
 #' @name coercer
+#'
+#' @section Usage:
+#' intonum(df = data, columns = c(1, 16:20))
 NULL
